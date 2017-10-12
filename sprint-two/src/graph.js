b0c4e4bbb3e7421cc799734f29ead17efebb9ce0
amboz <abozigian@gmail.com>
2017-10-12 15:59:04 -0700

    implemented tree and graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,39 +1,63 @@
-
-
-// Instantiate a new graph
 var Graph = function() {
+  this.nodes = {};
+  this.edges = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.nodes[node] = node;
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return this.nodes[node] ? true : false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  if (this.contains(node)) {
+    delete this.nodes[node];
+    delete this.edges[node];
+  }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  if (_.indexOf(this.edges[fromNode], toNode) !== -1 || _.indexOf(this.edges[toNode], fromNode) !== -1) {
+    return true;
+  }
+
+  return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.edges[fromNode] = [fromNode, toNode];
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  if (this.hasEdge(fromNode, toNode)) {
+    delete this.edges[fromNode];
+  }
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  _.each(this.nodes, function(node) {
+    cb(node);
+  })
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * addNode: Constant, O(1)
+ * contains: Constant, O(1)
+ * removeNode: Constant, O(1)
+ * hasEdge: Linear, O(n)
+ * addEdge: Constant, O(1)
+ * removeEdge: Linear, O(n)
+ * forEachNode: Linear, O(n)
  */
 
-
+//Notes: 
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,10 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
+  _.extend(newTree, treeMethods);
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -11,13 +12,35 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var added = Tree(value);
+  this.children.push(added);
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+
+  return _.reduce(this.children, function(acc, item) {
+    if (item.contains(target)) {
+      return true;
+    }
+
+    return acc;
+  }, false)
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * Adding: Constant, O(1)
+ * Searching (DFS): Linear, O(n) 
  */
+
+//Notes: Fxn Shared (using extend to add shared methods to nodes)
+//Notes: Tree class with props:
+  //.children (array with subtrees)
+  //.addChild(value) method --> takes value, sets as target of a node, 
+    //adds that node as a child of the tree
+  //.contains(target) method --> bool if found in target node or any descendants nodes
